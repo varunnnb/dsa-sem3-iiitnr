df69c8247686636f74d98dca39bbf2c2afc25663
varunnnb <pinky96nb@gmail.com>
2025-09-08 12:37:03 +0530

    lab6

---
+++ lab6/lab6-1.c
@@ -0,0 +1,85 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+char stack[100][100];
+int top = -1;
+
+void push(char *str)
+{
+    if (top < 99)
+    {
+        strcpy(stack[++top], str);
+    }
+    else
+    {
+        printf("overflow\n");
+    }
+}
+
+char *pop()
+{
+    if (top >= 0)
+    {
+        return stack[top--];
+    }
+    else
+    {
+        printf("underflow\n");
+        return 0;
+    }
+}
+
+int isOperator(char c)
+{
+    return (c == '+' || c == '-' || c == '*' || c == '/');
+}
+
+int isOperand(char c)
+{
+    return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'));
+}
+
+char toLowerCase(char c)
+{
+    if (c >= 'A' && c <= 'Z')
+    {
+        return c + 32;
+    }
+    return c;
+}
+
+int main()
+{
+    char postfix[100];
+    printf("enter postfix expression: ");
+    fgets(postfix, sizeof(postfix), stdin);
+
+    postfix[strcspn(postfix, "\n")] = 0;
+
+    for (int i = 0; i < strlen(postfix); i++)
+    {
+        char c = postfix[i];
+
+        if (c == ' ' || c == '\t')
+            continue;
+
+        if (isOperand(c))
+        {
+            char op[2] = {toLowerCase(c), '\0'};
+            push(op);
+        }
+        else if (isOperator(c))
+        {
+            char op1[100], op2[100], expr[100];
+            strcpy(op2, pop());
+            strcpy(op1, pop());
+            sprintf(expr, "%c%s%s", c, op1, op2);
+            push(expr);
+        }
+    }
+
+    printf("prefix expression: %s\n", stack[top]);
+
+    return 0;
+}
---
+++ lab6/lab6-2.c
@@ -0,0 +1,86 @@
+#include <stdio.h>
+#include <string.h>
+
+char stack[100];
+int top = -1;
+
+void push(char c)
+{
+    if (top < 99)
+    {
+        stack[++top] = c;
+    }
+    else
+    {
+        printf("overflow\n");
+    }
+}
+
+char pop()
+{
+    if (top >= 0)
+    {
+        return stack[top--];
+    }
+    else
+    {
+        return '\0';
+    }
+}
+
+int isMatching(char open, char close)
+{
+    if (open == '(' && close == ')')
+        return 1;
+    if (open == '{' && close == '}')
+        return 1;
+    if (open == '[' && close == ']')
+        return 1;
+    return 0;
+}
+
+int checkBalanced(char expr[])
+{
+    for (int i = 0; i < strlen(expr); i++)
+    {
+        char c = expr[i];
+
+        if (c == '(' || c == '{' || c == '[')
+        {
+            push(c);
+        }
+        else if (c == ')' || c == '}' || c == ']')
+        {
+            char popped = pop();
+            if (!isMatching(popped, c))
+            {
+                return 0;
+            }
+        }
+    }
+
+    if (top == -1)
+        return 1;
+    else
+        return 0;
+}
+
+int main()
+{
+    char expr[100];
+
+    printf("enter expression: ");
+    fgets(expr, sizeof(expr), stdin);
+    expr[strcspn(expr, "\n")] = 0;
+
+    if (checkBalanced(expr))
+    {
+        printf("balanced\n");
+    }
+    else
+    {
+        printf("not balanced\n");
+    }
+
+    return 0;
+}
